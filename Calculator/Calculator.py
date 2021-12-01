5e01959d4cf08269e4fdf19478f6ef982a6f360f
AishRahatal <aishwarya.rahatal97@gmail.com>
2021-12-01 09:51:36 +0530

    [Aishwarya] Add: functions which are performing basic arithmatic operations

---
+++ Calculator/Calculator.py
@@ -0,0 +1,50 @@
+'''	
+@Author: Aishwarya
+@Date: 2021-11-29
+@Title : Calculator
+'''
+########################################################################################################
+
+class Calculator:
+
+    def __init__(self):
+        """
+        Description: It is a constructor which initialize object
+        Parameter: self
+        Return: none
+        """
+        pass
+
+    def add(self, a, b):
+        """
+        Description:This function perform addition of two numbers
+        Parameter: self,a,b
+        Return: a+b
+        """
+        return a + b
+
+    def sub(self, a, b):
+        """
+        Description:This function perform subtraction of two numbers
+        Parameter: self,a,b
+        Return: a-b
+        """
+
+        return a - b
+  
+    def mul(self, a, b):
+        """
+        Description:This function perform multiplication of two numbers
+        Parameter: self,a,b
+        Return: a*b
+        """
+        return a * b
+
+    def div(self, a, b):
+        """
+        Description:This function perform division of two numbers
+        Parameter: self,a,b
+        Return: a/b
+        """
+        #if b != 0:
+        return a / b
